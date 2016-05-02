009e71b902ce53f132fe14aab89801564091edde
Daelin Fischman <dfischman1@gmail.com>
2016-05-02 18:46:58 -0400

    beginning the read

--- lab7.cpp
+++ lab7.cpp
@@ -1,10 +1,31 @@
-#include <direct>
 #include "lab7.h"
 
-using namespace std;
+
+vector< char[1024]> blockBuffer;
+
+int lab7::import(char * filename, char * lfs_filename)
+{
+
+    ifstream inputFile(filename, ifstream::in);
+    if(inputFile)
+    {
+        char * kbBlock = new char [1024];
+        while(inputFile.get(kbBlock,1025,EOF) && blockBuffer.size()< 1024)
+        {
+            blockBuffer.push_back(kbBlock);
+        }
+        for(int i=0; i <blockBuffer.size(); i++)
+        {
+            cout <<blockBuffer[i] <<endl;
+        }
+    }    
+}
 
 int main(int argc, char **argv)
 {
-
 	return 0;
 }
+
+
+
+
--- lab7.h
+++ lab7.h
@@ -1,6 +1,11 @@
 #ifndef LAB7_H_
 #define LAB7_H_
+#include <iostream>
+#include <fstream>
+#include <vector>
 
-
-
+class lab7{
+    private:
+    int import(char * filename, char * lfs_filename);
+};
 #endif
