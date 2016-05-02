57b41702498769ac17385e753416ff439f0e9855
jasperandrew <jasper@jasperandrew.com>
2016-05-02 17:44:36 -0400

    BEGIN

--- lab7.cpp
+++ lab7.cpp
@@ -1,1 +1,10 @@
-#include "lab7.h""
+#include <direct>
+#include "lab7.h"
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+
+	return 0;
+}
--- lab7.h
+++ lab7.h
@@ -1,9 +1,6 @@
 #ifndef LAB7_H_
 #define LAB7_H_
 
-class LFS
-{
- public:
 
- private:
-};
+
+#endif
