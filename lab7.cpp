0037a54428e68979f8fcfc70f18d61844b9a9c48
jasperandrew <jasper@jasperandrew.com>
2016-05-02 18:45:24 -0400

    making directories

--- lab7.cpp
+++ lab7.cpp
@@ -1,10 +1,12 @@
-#include <direct>
 #include "lab7.h"
-
-using namespace std;
 
 int main(int argc, char **argv)
 {
-
+	string path = "DRIVE";
+	mkdir(path.c_str(), S_IRWXU | S_IRWXG | S_IRWXO);
+	path += "/SEGMENT";
+	for(int i = 0; i < 32; i++){
+		mkdir((path + to_string(i+1)).c_str(), S_IRWXU | S_IRWXG | S_IRWXO);
+	}
 	return 0;
 }
--- lab7.h
+++ lab7.h
@@ -1,6 +1,10 @@
 #ifndef LAB7_H_
 #define LAB7_H_
 
+#include <iostream>
+#include <sys/stat.h>
+#include <string>
 
+using namespace std;
 
 #endif
