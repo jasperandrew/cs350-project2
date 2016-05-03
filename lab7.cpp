5bdbe99748b5752c5ab0e905c88eaee589eb7f78
jasperandrew <jasper@jasperandrew.com>
2016-05-02 20:31:57 -0400

    something

--- lab7.cpp
+++ lab7.cpp
@@ -6,7 +6,9 @@ int main(int argc, char **argv)
 	mkdir(path.c_str(), S_IRWXU | S_IRWXG | S_IRWXO);
 	path += "/SEGMENT";
 	for(int i = 0; i < 32; i++){
-		mkdir((path + to_string(i+1)).c_str(), S_IRWXU | S_IRWXG | S_IRWXO);
+		FILE *fp = fopen((path + to_string(i+1)).c_str(), "w");
+		ftruncate(fileno(fp), 1024*1024);
+		fclose(fp);
 	}
 	return 0;
 }
--- lab7.h
+++ lab7.h
@@ -1,8 +1,11 @@
 #ifndef LAB7_H_
 #define LAB7_H_
 
-#include <iostream>
+#include <unistd.h>
 #include <sys/stat.h>
+#include <sys/types.h>
+
+#include <iostream>
 #include <string>
 
 using namespace std;
