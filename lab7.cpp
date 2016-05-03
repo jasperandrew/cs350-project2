87e4c43b0097b154f763f457e5a57ea523aecd72
jasperandrew <jasper@jasperandrew.com>
2016-05-03 09:25:23 -0400

    minor fixes

--- lab7.cpp
+++ lab7.cpp
@@ -14,39 +14,40 @@ int initDrive()
 }
 
 vector<char*> blockBuffer;
-int import(char * filename, char * lfs_filename)
+int import(char *filename, char *lfs_filename)
 {
-    ifstream inputFile(filename, ifstream::in);
-    if(inputFile)
-    {
-        char * kbBlock = new char [1024];
-        while(inputFile.get(kbBlock,1025,EOF) && blockBuffer.size()< 1024)
-        {
-            blockBuffer.push_back(kbBlock);
-        }
-        for(int i=0; i <blockBuffer.size(); i++)
-        {
-            cout <<blockBuffer[i] <<endl;
-        }
-    }    
+	ifstream inputFile(filename, ifstream::in);
+	if(inputFile)
+	{
+		char *kbBlock = new char[1024];
+		while(inputFile.get(kbBlock, 1025, EOF) && blockBuffer.size() < 1024)
+		{
+			blockBuffer.push_back(kbBlock);
+		}
+		for(int i = 0; i < blockBuffer.size(); i++)
+		{
+			cout << blockBuffer[i] << endl;
+		}
+	}
 }
 
 int main(int argc, char **argv)
 {
 	int reload = initDrive();
+	
 	//argument parsing
 	string command;
 	vector<string> args;
 	getline(cin, command);
 	split(args, command, is_any_of(" "));
 	while(!command.empty())
-	  {
-	    if(command == "import")
-	      {
-		cout << "importing file " <<  command << " lfs file\n";
-		//import(arg1, arg2);                                                 
-	      }
-	    getline(cin, command, ' ');
-	  }
+	{
+		if(command == "import")
+		{
+			cout << "importing file " << command << " lfs file\n";
+			//import(arg1, arg2);                                                 
+		}
+		getline(cin, command, ' ');
+	}
 	return 0;
 }
