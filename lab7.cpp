2f410848c88a7b7342947de0e2fcb4d311559893
Jacob Zwickler <jzwickl1@cs.binghamton.edu>
2016-05-11 19:19:45 -0400

    overwrite update

--- lab7.cpp
+++ lab7.cpp
@@ -61,7 +61,7 @@ int main(int argc, char **argv)
 			else if((args[0] == "display" || args[0] == "d") && args.size() == 4)
                           {
                             cout<<"displaying...\n";
-                            display(args[1],args[2], args[3]);
+			    // display(args[1],args[2], args[3]);
                           }
 			else if((args[0] == "help" || args[0] == "h"))
 			{
